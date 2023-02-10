125b45ebce1f982a34f3797012655e9bfe708907
lucas moltedo <lucasmoltedo03@gmail.com>
2023-02-10 20:36:26 -0300

    Gran avance en la navbar, se agregó JavaScript

---
+++ js/general.js
@@ -0,0 +1,18 @@
+/***********************************************
+
+Author: Lucas Moltedo
+
+***********************************************/
+
+const menuButton = document.querySelector('.menu-button');
+
+// querySelector nos permite seleccionar un elemento
+// Lo que encuentre se guardará en una const (constante) llamada menuButton
+
+// Agregamos un evento al elemento para cuando se le haga click:
+menuButton.addEventListener('click', function () {
+    // Con "getElementById" seleccionamos un elemento por un ID:
+    document.getElementById('menu-options-movile').classList.toggle('active')
+    // Con ".classList.toggle()" le podemos agregar/quitar una clase (depende si la tiene), a un elemento
+    // Con esto ya tenemos la función de mostrar/ocultar el menú
+})
